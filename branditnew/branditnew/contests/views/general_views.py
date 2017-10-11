10aaa32ce69fbb6a0724d1077e0678ef28274ee6
Alvin Ofori <hagiosofori@gmail.com>
2017-10-11 10:14:34 +0000

    successfully installed admin starter page

--- branditnew/branditnew/contests/views/general_views.py
+++ branditnew/branditnew/contests/views/general_views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.template import loader
+
 
 
 
 def custom_admin(request):
-    return
+    template = loader.get_template('contests/custom_admin.html')
+    return HttpResponse(template.render())
